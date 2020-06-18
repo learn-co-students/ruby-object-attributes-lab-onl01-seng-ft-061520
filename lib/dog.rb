1baf4759ec2aeec36324c3c032819f9c6eaefa17
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-18 23:08:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,24 +5,20 @@ class Dog
   def dog(name)
     @name = name
   end
+      def name
+        @name
+  end
  
-    def name
-      @name
-    end
- 
-      def name= (pet_name)
-        @name = pet_name
-    end
+  def name= (pet_name)
+    @name = pet_name
+  end
 end
  
-  def dog(breed)
+  def breed(breed)
     @breed = breed 
+  end 
+    def breed 
+      @breed
     end 
-
-  def breed 
-    @breed 
-      end 
-      
-    def breed= (breed_name)
-      @breed = breed_name
-    end 
+ def breed= (breed_name)
+   @breed = breed_name 
