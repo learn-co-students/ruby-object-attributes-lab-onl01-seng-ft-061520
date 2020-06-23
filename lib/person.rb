c607c9c644d4248750492cb2894b7fd4cea3c993
Lisa Feng <lisafeng10@gmail.com>
2020-06-23 14:14:58 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(a_job)
+    @job = a_job
+  end
+  
+  def job
+    @job
+  end
+end
