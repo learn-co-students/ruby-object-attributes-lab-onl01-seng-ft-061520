ee8a602601424747bd51378c3dcdf21bcdc7aba1
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 03:10:56 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+  def job
+    @job
+  end
+end
+
+
+
+
+
+
+# Person with names #name= writes the name of the person to an instance variable @name
+# Person with names #name reads the name of the person from an instance variable @name
+# Person with jobs #job= writes the job of the person to an instance variable @job
+# Person with jobs #job reads the job of the person from an instance variable @job
