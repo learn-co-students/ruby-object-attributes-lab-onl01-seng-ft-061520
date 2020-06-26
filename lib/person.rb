3dac271beb622ea3843c41623a0d9b5741b60bd2
Jose Hidago <jhidalgo521@gmail.com>
2020-06-26 12:22:01 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  #name setter
+  def name= (person_name)
+    @name = person_name
+  end 
+  
+  #name getter
+  def name
+    @name
+  end
+  
+  #job setter
+  def job= (job_title)
+    @job = job_title
+  end
+  
+  #job getter
+  def job 
+    @job
+  end 
+  
+end 
